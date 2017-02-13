d387e4e39ae3af6626b40cde4b1150f53849e79b
Jeff Schornick <jesc5667@colorado.edu>
2017-02-12 23:24:05 -0700

    Add sqrt_approx template project

---
+++ m1/sqrt_approx/main.c
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------
+LAB EXERCISE 5.2 - SQUARE ROOT APPROXIMATION
+ ----------------------------------------
+Write an assembly code subroutine to approximate the square root of an 
+argument using the bisection method. All math is done with integers, so the 
+resulting square root will also be an integer
+
+GOOD LUCK!
+ *----------------------------------------------------------------------------*/
+
+__asm int my_sqrt(int x){
+	
+	//Write your code here
+
+}
+
+/*----------------------------------------------------------------------------
+ MAIN function
+ *----------------------------------------------------------------------------*/
+int main(void){
+	volatile int r, j=0;
+	int i;
+  r = my_sqrt(0);     // should be 0
+  r = my_sqrt(25);    // should be 5
+	r = my_sqrt(133); 	// should be 11
+  for (i=0; i<10000; i++){
+		r = my_sqrt(i);
+    j+=r;
+  }
+	while(1)
+		;
+}
+
+// *******************************ARM University Program Copyright © ARM Ltd 2014*************************************
